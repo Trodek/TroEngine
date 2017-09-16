72e66cf35c1cfe190297dd048ed64740a350110e
Trodek <ibanmas@gmail.com>
2017-09-16 17:47:51 +0200

    Base Engine Code

---
+++ TroEngine/Engine/ModuleSceneIntro.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "Primitive.h"
+
+struct PhysBody3D;
+struct PhysMotor3D;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
+public:
+	
+};
